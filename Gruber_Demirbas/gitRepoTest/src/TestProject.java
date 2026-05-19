1440c8a8b7576a1132e62fad4c605e88c8b1374e
EmrysIX <7t9gf8mb9r@privaterelay.appleid.com>
2026-05-19 17:39:06 +0200

    new println text member 2

--- Gruber_Demirbas/gitRepoTest/src/TestProject.java
+++ Gruber_Demirbas/gitRepoTest/src/TestProject.java
@@ -1,10 +1,10 @@
 public class TestProject {
-<<<<<<< HEAD
-    public void thisIsANewMethod() { }
-=======
+    public void thisIsANewMethod() {
+        System.out.println("Member 2 Text");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 
->>>>>>> 9b05ebd01b085ec427b411e855171eec1a26b77c
 }
