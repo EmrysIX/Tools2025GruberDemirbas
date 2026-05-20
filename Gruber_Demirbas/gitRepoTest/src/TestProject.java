eca4091a801089e027c99f4313b8cb7049a54090
Gregor Gruber <s2510237007@fhooe.at>
2026-05-20 17:02:31 +0200

    Member1 added line1

--- Gruber_Demirbas/gitRepoTest/src/TestProject.java
+++ Gruber_Demirbas/gitRepoTest/src/TestProject.java
@@ -2,6 +2,7 @@ public class TestProject {
 
     public void thisIsANewMethod() {
         System.out.println("Member1:This is a new method");
+        System.out.println("Member1:This is another method");
     }
 
     public static void main(String[] args) {
